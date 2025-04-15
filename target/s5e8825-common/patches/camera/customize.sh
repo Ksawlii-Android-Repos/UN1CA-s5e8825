10bc488113e1e6374debbef0b4846268b02fa049
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-15 09:12:26 +0200

    s5e8825-common: Fix AI Photo Editor
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/s5e8825-common/patches/camera/customize.sh
+++ target/s5e8825-common/patches/camera/customize.sh
@@ -52,3 +52,14 @@ if [ "$TARGET_CODENAME" = "a53x" ]; then
 elif [ "$TARGET_CODENAME" = "a25x" ]; then
     sed -i '/libimage_enhancement.arcsoft.so/d' "$WORK_DIR/system/system/etc/public.libraries-arcsoft.txt"
 fi
+
+echo "Fix AI Photo Editor"
+cp -a --preserve=all \
+    "$SRC_DIR/prebuilts/samsung/a52qnsxx/system/cameradata/portrait_data/single_bokeh_feature.json" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/unica_bokeh_feature.json"
+SET_METADATA "system" "system/cameradata/portrait_data/unica_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
+sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json"
+sed -i \
+    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/unica_bokeh_feature.json\x00/g' \
+    "$WORK_DIR/system/system/lib64/libPortraitSolution.camera.samsung.so"
