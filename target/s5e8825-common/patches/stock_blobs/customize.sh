98e2027ce44b14fc84548b1aa5af26ec4afff29e
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-20 15:28:10 +0200

    s5e8825-common: stock_blobs: Fix DeX
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/s5e8825-common/patches/stock_blobs/customize.sh
+++ target/s5e8825-common/patches/stock_blobs/customize.sh
@@ -38,3 +38,15 @@ elif [ "$TARGET_CODENAME" = "a25x" ]; then
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundBooster_ver1130.so" 0 0 644 "u:object_r:system_lib_file:s0"
     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1130.so" 0 0 644 "u:object_r:system_lib_file:s0"
 fi
+
+echo "Fix DeX"
+DELETE_FROM_WORK_DIR "system" "system/bin/remotedisplay"
+DELETE_FROM_WORK_DIR "system" "system/lib/libremotedisplay.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplay.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplay_wfd.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libremotedisplayservice.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay_wfd.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplayservice.so" 0 0 644 "u:object_r:system_lib_file:s0"
