0a1ac4b896e6de7af76de167bc47dbcbdfd2a348
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-20 15:24:29 +0200

    s5e8825-common: Fix live wallpapers
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- unica/mods/wallpaper/customize.sh
+++ unica/mods/wallpaper/customize.sh
@@ -14,23 +14,29 @@ RESIZE_APK()
     fi
 
     echo "Resize wallpaper-res.apk"
-    DECODE_APK "/system/priv-app/wallpaper-res/wallpaper-res.apk"
+    if [ ! -d "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk" ]; then
+        DECODE_APK "/system/priv-app/wallpaper-res/wallpaper-res.apk"
+    fi
     for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/drawable-nodpi/Wallpaper_"*.webp
     do
         cwebp -q 100 -resize $WEBP_RES $WEBP_RES "$f" -o "$(dirname "$f")/temp.webp" &> /dev/null
         mv -f "$(dirname "$f")/temp.webp" "$f"
     done
-    for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/"*.mp4
-    do
-        ffmpeg -i "$f" -c:v libx265 -tag:v hvc1 -c:a copy \
-            -crf 18 -movflags use_metadata_tags -map_metadata 0 \
-            -vf "scale=$MP4_RES,setsar=1:1" -video_track_timescale 60000 \
-            "$(dirname "$f")/temp.mp4" &> /dev/null
-        mv -f "$(dirname "$f")/temp.mp4" "$f"
-    done
+    if [ ! "$TARGET_HAS_COMMON_TARGET" = "s5e8825-common" ]; then
+        for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/"*.mp4
+        do
+            ffmpeg -i "$f" -c:v libx265 -tag:v hvc1 -c:a copy \
+                -crf 18 -movflags use_metadata_tags -map_metadata 0 \
+                -vf "scale=$MP4_RES,setsar=1:1" -video_track_timescale 60000 \
+                "$(dirname "$f")/temp.mp4" &> /dev/null
+            mv -f "$(dirname "$f")/temp.mp4" "$f"
+        done
+    fi
 }
 # ]
 
-ADD_TO_WORK_DIR "e1qzcx" "system" \
-    "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
+if [ ! -d "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk" ]; then
+    ADD_TO_WORK_DIR "e1qzcx" "system" \
+        "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
+fi
 RESIZE_APK
