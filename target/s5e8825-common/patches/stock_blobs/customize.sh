e14b1fdcfc89fc72243b03424cdde849cff1c740
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-20 15:28:24 +0200

    s5e8825-common: stock_blobs: Fix Nfc
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/s5e8825-common/patches/stock_blobs/customize.sh
+++ target/s5e8825-common/patches/stock_blobs/customize.sh
@@ -50,3 +50,7 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libremotedisplay.so" 0 0
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplay_wfd.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libremotedisplayservice.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+echo "Fix NFC"
+DELETE_FROM_WORK_DIR "system" "system/app/NfcNci"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/app/NfcNci" 0 0 755 "u:object_r:system_file:s0"
