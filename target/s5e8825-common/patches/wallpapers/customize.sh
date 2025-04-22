5bc6bfa8715a015e1985665aada6f1d597bf3ac8
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-22 14:04:07 +0200

    s5e8825-common: wallpapers: Fix copying wallpapers
    cp: cannot stat 'target/s5e8825-common/patches/wallpapers/mp4/*': No such file or directory
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/s5e8825-common/patches/wallpapers/customize.sh
+++ target/s5e8825-common/patches/wallpapers/customize.sh
@@ -1,8 +1,5 @@
 echo "Fix Live Wallpapers"
 ADD_TO_WORK_DIR "e1qzcx" "system" "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
 DECODE_APK "/system/priv-app/wallpaper-res/wallpaper-res.apk"
-cp -fa "target/$TARGET_COMMON_NAME/patches/wallpapers/mp4/Default_Video_Wallpaper_ZALG.mp4" \
-      "target/$TARGET_COMMON_NAME/patches/wallpapers/mp4/Default_Video_Wallpaper_ZK.mp4" \
-      "target/$TARGET_COMMON_NAME/patches/wallpapers/mp4/Default_Video_Wallpaper_ZVLB.mp4" \
-      "target/$TARGET_COMMON_NAME/patches/wallpapers/mp4/Default_Video_Wallpaper_ZYZO.mp4" \
+cp -fa "$SRC_DIR/target/$TARGET_COMMON_NAME/patches/wallpapers/mp4/"* \
       "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/"
