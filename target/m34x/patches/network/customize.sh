9f946860bd9a014a119b250796c60d3ecec62fda
Ksawlii <ksawery.blaszczak@proton.me>
2025-05-23 20:06:03 +0200

    target: s5e8825: patches: network: fixup
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/m34x/patches/network/customize.sh
+++ target/m34x/patches/network/customize.sh
@@ -1,6 +1,6 @@
 echo "Improve WiFi/Mobile Data speeds"
 
-DELETE_FROM_WORK_DIR "product" "overlay/ConnectivityUxOverlay"
-DELETE_FROM_WORK_DIR "product" "overlay/NetworkStackOverlay"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "overlay/ConnectivityUxOverlay" 0 0 755 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "overlay/NetworkStackOverlay" 0 0 755 "u:object_r:system_file:s0"
+DELETE_FROM_WORK_DIR "product" "app/ConnectivityUxOverlay"
+DELETE_FROM_WORK_DIR "product" "app/NetworkStackOverlay"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "app/ConnectivityUxOverlay" 0 0 755 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "app/NetworkStackOverlay" 0 0 755 "u:object_r:system_file:s0"
